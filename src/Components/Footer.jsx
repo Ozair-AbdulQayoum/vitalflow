74a4982bedb64e638f9068b42a3aee2df0f2bea2
Ozair Abdul Qayoum <ozairabdulqayoum@gmail.com>
2026-06-14 14:08:00 +0430

    Fix footer logo path

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/logo.png";
+
 const footerLinks = [
   {
     title: "Static",
@@ -21,22 +21,21 @@ const footerLinks = [
 
 const Footer = () => {
   return (
-    <footer className="bg-white-900 text-black px-6 py-12">
+    <footer className="bg-white text-black px-6 py-12">
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-10">
         {/* LEFT SIDE */}
         <div>
-          {/* Logo */}
           <div className="flex items-center gap-2">
-            <img src={logo} alt="Logo" className="w-10 h-10" />
-            <h3 className="text-1xl font-bold text-black-600">Vital+Flow</h3>
+            <img src="/logo.png" alt="Logo" className="w-10 h-10" />
+            <h3 className="text-xl font-bold">Vital+Flow</h3>
           </div>
 
-          <p className="text-gray-400 mt-3">
+          <p className="text-gray-600 mt-3">
             We build modern web applications with clean UI and strong
             performance.
           </p>
 
-          <button className="mt-5 bg-blue-600 hover:bg-blue-700 px-5 py-2 rounded-lg transition">
+          <button className="mt-5 bg-blue-600 hover:bg-blue-700 px-5 py-2 rounded-lg transition text-white">
             Get Started
           </button>
         </div>
@@ -49,28 +48,26 @@ const Footer = () => {
             <input
               type="email"
               placeholder="Enter your email"
-              className="w-full p-2 rounded-l-lg text-black outline-none"
+              className="w-full p-2 rounded-l-lg text-black outline-none border"
             />
-            <button className="bg-green-600 px-4 rounded-r-lg hover:bg-green-700">
+            <button className="bg-green-600 px-4 rounded-r-lg hover:bg-green-700 text-white">
               Subscribe
             </button>
           </div>
 
-          <p className="text-gray-400 mt-3 text-sm">
+          <p className="text-gray-500 mt-3 text-sm">
             Stay updated with our latest news and updates.
           </p>
 
-          {/* LINKS */}
           <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mt-8">
             {footerLinks.map((section, index) => (
               <div key={index}>
                 <h5 className="font-semibold mb-3">{section.title}</h5>
-
-                <ul className="space-y-2 text-gray-400">
+                <ul className="space-y-2 text-gray-500">
                   {section.links.map((link, i) => (
                     <li
                       key={i}
-                      className="hover:text-gray cursor-pointer transition"
+                      className="hover:text-black cursor-pointer transition"
                     >
                       {link}
                     </li>
